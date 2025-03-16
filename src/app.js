0ca6578f50460508f8f8cd77c332fbc753dc568c
Ramanjaneyulu2805 <pandu28052001@gmail.com>
2025-03-16 22:41:17 +0530

    HTTP_Methods

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 
 const app = express();
+
+app.get('/user/:id/:name',(req,res)=>{
+    console.log(req.params)
+    res.send({firstName:"rama",LastName:"Roy"})
+})
+
+
+app.post('/user',(req,res)=>{
+    res.send("Data successfuly send to the database")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send('deleted successfully')
+})
 //created a new webserver
 //you need to have a port number
 
@@ -9,6 +23,7 @@ const app = express();
 app.use('/test',(req,res)=>{
     res.send("Hello I am from serverI am sitting on port 3000")
 })
+//app.use will match all HTTP methods 
 
 app.listen(3000,()=>{console.log("successfully listening on port 3000")});
 
